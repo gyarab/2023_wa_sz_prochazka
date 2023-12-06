bd1450d4ff6518e4ced49e96e4688d9ca432d703
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2023-12-06 18:38:02 +0100

    Working Card changebility

--- pexeso.js
+++ pexeso.js
@@ -1,25 +1,34 @@
-let el = document.getElementById('gamestate')
-el.innerHTML = "new game"
-let counter = 5
+// let el = document.getElementById('gamestate')
+// el.innerHTML = "new game"
+// let counter = 5
 
-function gameCountDown() {
-   el.innerHTML = counter
-   counter --
-   if (counter > 0 ){
-    setTimeout(gameCountDown, 1000)
-   }
-   //nefunkcni
-   else{
-    counter = "hraj ty cokolado"
-    setTimeout(gameCountDown, 1000)
-   }
-}
-setTimeout(gameCountDown, 1000)
-el.innerHTML = "newgame"
+// function gameCountDown() {
+//    el.innerHTML = counter
+//    counter --
+//    if (counter > 0 ){
+//     setTimeout(gameCountDown, 1000)
+//    }
+//    //nefunkcni
+//    else{
+//     counter = "hraj ty cokolado"
+//     setTimeout(gameCountDown, 1000)
+//    }
+// }
+// setTimeout(gameCountDown, 1000)
+// el.innerHTML = "newgame"
 
-let card1 = document.getElementById('card-1')
-let card2 = document.getElementById('card-2')
+// let card1 = document.getElementById('card-1')
+// let card2 = document.getElementById('card-2')
 
-function clickCard(){
-    
+var img_list = ["pexesoIMG/gragas1.jpg","pexesoIMG/gragas2.jpg","pexesoIMG/gragas3.jpg","pexesoIMG/gragas4.jpg","pexesoIMG/gragas5.jpg","pexesoIMG/gragas6.jpg"]
+function change_card(cardID){
+    console.log(cardID)
+    var card = document.getElementById(cardID);
+    var cardImg = card.getElementsByTagName("img")[0];
+    var imgIndex = cardImg.dataset.imgindex
+    if (cardImg.src.endsWith("pexesoIMG/lol.jpg")) {
+        cardImg.src = img_list[imgIndex];
+    } else {
+        cardImg.src = "pexesoIMG/lol.jpg";
+    }
 }
