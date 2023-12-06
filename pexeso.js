fe669b826a390ca71baa200d3604b0f3b4ff2414
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2023-12-06 18:49:53 +0100

    update

--- pexeso.js
+++ pexeso.js
@@ -1,26 +1,5 @@
-// let el = document.getElementById('gamestate')
-// el.innerHTML = "new game"
-// let counter = 5
+var img_list = ["pexesoIMG/gragas0.jpg","pexesoIMG/gragas1.jpg","pexesoIMG/gragas2.jpg","pexesoIMG/gragas3.jpg","pexesoIMG/gragas4.jpg","pexesoIMG/gragas5.jpg"]
 
-// function gameCountDown() {
-//    el.innerHTML = counter
-//    counter --
-//    if (counter > 0 ){
-//     setTimeout(gameCountDown, 1000)
-//    }
-//    //nefunkcni
-//    else{
-//     counter = "hraj ty cokolado"
-//     setTimeout(gameCountDown, 1000)
-//    }
-// }
-// setTimeout(gameCountDown, 1000)
-// el.innerHTML = "newgame"
-
-// let card1 = document.getElementById('card-1')
-// let card2 = document.getElementById('card-2')
-
-var img_list = ["pexesoIMG/gragas1.jpg","pexesoIMG/gragas2.jpg","pexesoIMG/gragas3.jpg","pexesoIMG/gragas4.jpg","pexesoIMG/gragas5.jpg","pexesoIMG/gragas6.jpg"]
 function change_card(cardID){
     console.log(cardID)
     var card = document.getElementById(cardID);
